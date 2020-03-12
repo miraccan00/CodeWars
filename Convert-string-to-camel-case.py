2ff87248b366f492ffe03a89a061f61b1401cbb2
Mirac Can Yilmaz <miraccan6161@gmail.com>
2020-03-12 08:00:37 +0300

    Create Convert-string-to-camel-case.py

---
+++ Convert-string-to-camel-case.py
@@ -0,0 +1,15 @@
+def to_camel_case():
+    text = "the-stealth-warrior"
+    if '_' in text:
+        my_splitted_text = text.split('_')
+        print(my_splitted_text)
+    elif '-' in text:
+        my_splitted_text = text.split('-')
+        for i in range(len(my_splitted_text)):
+            my_splitted_text = my_splitted_text.  my_splitted_text[i][0]
+
+
+"""tamamlanmadı"""
+
+
+to_camel_case()

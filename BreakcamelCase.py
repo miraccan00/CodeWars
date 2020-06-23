c36ee548fd176a4d0fbf21c6b865f1a54ef11bf2
Mirac Can Yılmaz <miraccan6161@gmail.com>
2020-06-23 19:53:22 +0300

    camelCaseProblemSolve

---
+++ BreakcamelCase.py
@@ -0,0 +1,11 @@
+def solution(s):
+    final_string = ""
+    for i in range(len(s)):
+        char = s[i]
+        if char.isupper():
+            final_string += " " + char
+        else:
+            final_string += char
+    return final_string
+
+print(solution("HelloWorld"))
